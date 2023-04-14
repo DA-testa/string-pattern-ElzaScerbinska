eab66a02f3256fb174f3c6b2a4d9aa1e210e3a5d
ElzaScerbinska <124616741+ElzaScerbinska@users.noreply.github.com>
2023-04-14 11:35:05 +0000

    -

--- hash_substring.py
+++ hash_substring.py
@@ -5,15 +5,13 @@ def read_input():
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     text = input()
     if "F" in text:
-        print("pirms")
         filename = input()
-        print("pec")
-        #if "a" not in filename:
-        path = "./tests/" + filename
-        with open(path, "r") as file:
-            pattern = file.readline().rstrip()
-            text = file.readline().rstrip()
-            return (pattern, text)
+        if "a" not in filename:
+            path = "./tests/" + filename
+            with open(path, "r") as file:
+                pattern = file.readline().rstrip()
+                text = file.readline().rstrip()
+                return (pattern, text)
     if "I" in text:
         pattern = input().rstrip()
         text = input().rstrip()
